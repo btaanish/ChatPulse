c5f29317b1d52270abb5fd94daca0bb7ea5d7c46
D-Limiter <>
2024-07-30 22:12:09 +0800

    Finalise Sidebar

--- whatsapp/src/Sidebar.js
+++ whatsapp/src/Sidebar.js
@@ -1,16 +1,17 @@
 import React from "react";
 import "./Sidebar.css";
+import SidebarChat from "./SidebarChat";
 import ChatIcon from "@mui/icons-material/Chat";
 import DonutLargeIcon from "@mui/icons-material/DonutLarge";
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import { Avatar, IconButton } from "@mui/material";
 
 function Sidebar() {
   return (
-    <div className="Sidebar">
-      <h1>I'm a Sidebar</h1>
+    <div className="sidebar">
       <div className="sidebar__header">
-        <Avatar src="https://stockcake.com/i/modern-office-interior_1158879_803137"/>
+        <Avatar src="https://images.stockcake.com/public/2/2/b/22b80449-e67a-4dc3-9976-edbbddfea55b/modern-office-interior-stockcake.jpg" />
         <div className="sidebar__headerRight">
           <IconButton>
             <DonutLargeIcon />
@@ -22,7 +23,17 @@ function Sidebar() {
             <MoreVertIcon />
           </IconButton>
         </div>
+      </div>
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+          <SearchOutlinedIcon />
+          <input placeholder="Search or start new chat" type="text" />
         </div>
+      </div>
+      <div className="sidebar__chats">
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
       </div>
     </div>
   );
