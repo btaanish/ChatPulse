4b3b1444b802907081d06ef780ee697ae20022ea
D-Limiter <>
2024-07-31 01:04:25 +0800

    Finalise frontend

--- whatsapp/src/Chat.js
+++ whatsapp/src/Chat.js
@@ -1,10 +1,63 @@
 import React from "react";
 import "./Chat.css";
+import { Avatar, IconButton } from "@mui/material";
+import SearchOutlined from "@mui/icons-material/SearchOutlined";
+import AttachFileIcon from "@mui/icons-material/AttachFile";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import InsertEmoticonIcon from "@mui/icons-material/InsertEmoticon";
 
 function Chat() {
   return (
     <div className="chat">
-      <h1>I am the chat</h1>
+      <div className="chat__header">
+        <Avatar />
+        <div className="chat__headerInfo">
+          <h3>Room Name</h3>
+          <p>Last seen at ... </p>
+        </div>
+
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFileIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="chat__body">
+        <p className="chat__message">
+          <span className="chat__name">James</span>
+          This is a message
+          <span className="chat__timestamp">
+            {new Date(message.timestamp?.toDate()).toUTCString()}
+          </span>
+        </p>
+        <p className="chat__message chat__receiver">
+          <span className="chat__name">James</span>
+          This is a message
+          <span className="chat__timestamp">
+            {new Date(message.timestamp?.toDate()).toUTCString()}
+          </span>
+        </p>
+      </div>
+      <div className="chat__footer">
+        <InsertEmoticonIcon />
+        <form>
+          <input
+            value={input}
+            onChange={(e) => SelectInput(e.target.value)}
+            placeholder="Type a message"
+            type="text"
+          />
+          <button onClick={sendMessage} type="submit">
+            Send a message
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
